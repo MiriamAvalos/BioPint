5d557de52485df487a95f90d589c59e2ebe8803f
Mirisaurio <Mirisaurio>
2025-05-27 18:40:16 -0600

    iconos al final

--- src/components/SocialAndAddress.jsx
+++ src/components/SocialAndAddress.jsx
@@ -1,3 +1,4 @@
+// src/components/SocialAndAddress.jsx
 import React from 'react';
 import { FaFacebook, FaInstagram, FaWhatsapp, FaYoutube } from 'react-icons/fa';
 import { FaTiktok } from 'react-icons/fa6';
@@ -5,23 +6,46 @@ import '../components/SocialAndAddress.css';
 
 function SocialAndAddress() {
   return (
-    <footer className="footer bg-dark text-white py-4">
-      <div className="container text-center">
+    
+    <footer className="footer">
+      
+      
+      <div className="container">
         <div className="mb-3">
-          <a href="https://www.facebook.com/profile.php?id=61574091243583" target="_blank" rel="noopener noreferrer" className="text-white mx-3 social-icon">
+          <a
+            href="https://www.facebook.com/profile.php?id=61574091243583"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white mx-3 social-icon"
+          >
             <FaFacebook size={24} />
           </a>
-          <a href="https://www.instagram.com/biopint5/" target="_blank" rel="noopener noreferrer" className="text-white mx-3 social-icon">
+          <a
+            href="https://www.instagram.com/biopint5/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white mx-3 social-icon"
+          >
             <FaInstagram size={24} />
           </a>
-          <a href="https://www.youtube.com/@Biopint" target="_blank" rel="noopener noreferrer" className="text-white mx-3 social-icon">
+          <a
+            href="https://www.youtube.com/@Biopint"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white mx-3 social-icon"
+          >
             <FaYoutube size={24} />
           </a>
-             <a href="https://www.tiktok.com/@biopint" target="_blank" rel="noopener noreferrer" className="text-white mx-3 social-icon">
+          <a
+            href="https://www.tiktok.com/@biopint"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white mx-3 social-icon"
+          >
             <FaTiktok size={24} />
           </a>
         </div>
-        <p className="mb-0">Dirección: Calle Ficticia 123, Ciudad, País</p>
+        
       </div>
     </footer>
   );
